1d1fe316ccb724b288a3067dbbc3fd7ca9571a7d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-06-28 19:16:42 +0530

    Allow using the header and library in C++.

--- include/hdrbg.h
+++ include/hdrbg.h
@@ -6,10 +6,18 @@
 #include <stddef.h>
 
 struct hdrbg_t;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 struct hdrbg_t *hdrbg_new(void);
 void hdrbg_renew(struct hdrbg_t *hd);
 bool hdrbg_gen(struct hdrbg_t *hd, bool prediction_resistance, uint8_t *r_bytes, size_t r_length);
 void hdrbg_delete(struct hdrbg_t *hd);
 void hdrbg_test(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // TFPF_HASH_DRBG_INCLUDE_HDRBG_H_
