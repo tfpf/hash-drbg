5b60e48bd59711169c54f2481568dde199d899f8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-06-28 18:54:11 +0530

    Added example program. Updated ignore files.

---
+++ examples/thread_safe.c
@@ -0,0 +1,16 @@
+#include <hdrbg.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(void)
+{
+    uint8_t r_bytes[64];
+    struct hdrbg_t *hd = hdrbg_new();
+    hdrbg_gen(hd, false, r_bytes, 64);
+    for(int i = 0; i < 64; ++i)
+    {
+        printf("%02"PRIx8, r_bytes[i]);
+    }
+    printf("\n");
+    hdrbg_delete(hd);
+}
