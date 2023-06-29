2a0d9de2631aeeaa8ba12a26203f207265b8acb8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-06-29 22:44:37 +0530

    Created a subdirectory for C examples.
    Added thread-unsafe example.

---
+++ examples/C/thread-safe.c
@@ -0,0 +1,20 @@
+#include <hdrbg.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+    uint8_t r_bytes[64];
+    struct hdrbg_t *hd = hdrbg_new(true);
+    for(int i = 0; i < 4; ++i)
+    {
+        hdrbg_gen(hd, false, r_bytes, 64);
+        for(int i = 0; i < 64; ++i)
+        {
+            printf("%02"PRIx8, r_bytes[i]);
+        }
+        printf("\n");
+    }
+    hdrbg_delete(hd);
+}
--- examples/C/thread-unsafe.c
+++ examples/C/thread-unsafe.c
@@ -1,19 +1,20 @@
 #include <hdrbg.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(void)
 {
     uint8_t r_bytes[64];
-    struct hdrbg_t *hd = hdrbg_new();
+    hdrbg_new(false);
     for(int i = 0; i < 4; ++i)
     {
-        hdrbg_gen(hd, false, r_bytes, 64);
+        hdrbg_gen(NULL, false, r_bytes, 64);
         for(int i = 0; i < 64; ++i)
         {
             printf("%02"PRIx8, r_bytes[i]);
         }
         printf("\n");
     }
-    hdrbg_delete(hd);
+    hdrbg_delete(NULL);
 }
