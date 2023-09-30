242d2fdf3c539f803060a1df950b29c26f03debd
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-09-30 08:04:54 +0530

    Moved code into functions.

--- run.sh
+++ run.sh
@@ -7,21 +7,34 @@ case $os in
     (Darwin | Linux);;
     (CYGWIN* | MINGW* | MSYS*) alias sudo='';;
     (*)
-        printf "Unknown OS. Edit 'run.sh' as necessary and retry.\n" >&2
+        printf "Unknown OS: '$os'. Edit 'run.sh' as necessary and retry.\n" >&2
         exit 1
     ;;
 esac
 
-# Uninstall.
-if [ "$1" = rm ]
-then
+uninstall()
+{
     im=build/install_manifest.txt
     if [ ! -f $im ]
     then
-        printf "Cannot find install manifest!\n" >&2
+        printf "Cannot find install manifest.\n" >&2
         exit 1
     fi
-    sudo rm -v $(cat build/install_manifest.txt)
+    sudo rm -v $(cat $im)
+}
+
+install()
+{
+    mkdir -p build
+    cd build
+    cmake ..
+    cmake --build . --parallel
+    sudo cmake --install .
+}
+
+if [ "$1" = rm ]
+then
+    uninstall
     exit
 fi
 
@@ -32,7 +45,4 @@ then
     git clone https://github.com/tfpf/hash-drbg.git $target
     cd $target
 fi
-mkdir -p build && cd build
-cmake ..
-cmake --build . --parallel
-sudo cmake --install .
+install
